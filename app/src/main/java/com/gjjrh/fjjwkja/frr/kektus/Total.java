e0e8d1b3c14ca692047e835de6b98d1a7dae1666
Andrey Murzin <lefthanderomg@gmail.com>
2018-03-07 11:23:43 +0300

    fix bug

--- app/src/main/java/com/gjjrh/fjjwkja/frr/kektus/Total.java
+++ app/src/main/java/com/gjjrh/fjjwkja/frr/kektus/Total.java
@@ -64,10 +64,10 @@ public class Total {
 	public static float         CARD_STRT_Y     = 396f;
 	public static float         CARD_BETWEEN_X  = 145f;
 	public static float         CARD_BETWEEN_Y  = 136f;	
-	
-	public static String[]      strIconName     =  {"icon_1",
-			"icon_2","icon_3","icon_4","icon_5",
-			"icon_6","icon_7","icon_8","icon_9","icon_10","icon_11"};
+
+	public static String[]      strIconName     =  {"character1",
+			"character2","character3","character4","character5",
+			"character6","character7","character8","character9","character10","character11"};
 	public static String        GAME_STATE      = "";
 
 	
